ec9c5a0861e8c81e4e00f1a5fd9b38c0437ac2d6
justinpastine <justin.pastine@gmail.com>
2023-07-18 15:22:04 -0400

    add days of the week data

---
+++ days-of-the-week.mjs
@@ -0,0 +1,31 @@
+const daysOfTheWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
+
+const myFavoriteDay = 'Saturday'
+
+const saturday = {
+  weekday: false,
+  good: true,
+  quote: 'Saturdays do indeed exist'
+}
+
+function randomDay() {
+  return daysOfTheWeek[Math.floor(Math.random() * daysOfTheWeek.length)]
+  
+}
+
+console.log(daysOfTheWeek)
+
+export {
+  daysOfTheWeek,
+  myFavoriteDay,
+  saturday,
+  randomDay
+}
