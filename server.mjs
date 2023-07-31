25ab0111af7163ca0da0c997c2f8b0b5e036ae50
justinpastine <justin.pastine@gmail.com>
2023-07-31 14:37:18 -0400

    add isEmail validator

--- server.mjs
+++ server.mjs
@@ -1,5 +1,8 @@
 import * as daysOfTheWeek from './days-of-the-week.mjs'
+import validator from 'validator'
 
 console.log(daysOfTheWeek.saturday.weekday);
+
+console.log(validator.isEmail('foo@bar.com'))
 
 
